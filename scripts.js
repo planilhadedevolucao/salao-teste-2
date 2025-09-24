1224dbdb97111f93bf37d24e356464739403e3d7
planilhadedevolucao <planilhadedevolucao@hotmail.com>
2025-09-23 22:35:14 -0300

    at 1
    at1

--- scripts.js
+++ scripts.js
@@ -1,47 +1,76 @@
 // Preloader
-window.addEventListener('load', function() { 
-    const preload = document.getElementById('preload'); 
-    const body = document.getElementById('body'); 
-     
+window.addEventListener("pageshow", (event) => {
+  const preloader = document.getElementById("preloader");
+  const body = document.getElementById("body");
 
-    // Hide preloader and show content 
-    this.setTimeout(() => {
-        
-    preload.style.display = 'none'; // Hide preloader
-    body.style.display = 'block';  // Show content
+  if (document.referrer && document.referrer.includes(window.location.origin)) {
+    preloader.style.display = "none";
+    body.style.display = "block";
+  } else {
+    setTimeout(() => {
+      preloader.style.display = "none";
+      body.style.display = "block";
     }, 2000);
-}); 
+  }
+});
+
+  
+
+    
 
 
 // Card slider
-new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 5,
+new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 5,
 
-    // Pagination bullets
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 2,
     },
-
-    // Responsive breakpoints
-    breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3
-        }
-    }
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
+ const imagens = document.querySelectorAll('.galeria img');
+    const modal = document.getElementById('modal');
+    const imagemModal = document.getElementById('imagemModal');
+    const fecharBtn = document.getElementById('fechar');
+
+    imagens.forEach(imagem => {
+      imagem.addEventListener('click', () => {
+        imagemModal.src = imagem.src;
+        modal.style.display = 'flex';
+      });
+    });
+
+    // Fecha o modal ao clicar no botão "x"
+    fecharBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+
+    // Também fecha se clicar fora da imagem
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
